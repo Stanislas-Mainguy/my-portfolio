f1505ec7a7f3acd2005321afee3b11bd49593527
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-09-18 14:22:25 +0200

    Nouvelle structure des dossiers et fichiers

---
+++ src/components/atoms/button/generalButton/GeneralButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./GeneralButton.css";
+
+function GeneralButton() {
+    return (
+        <div>
+            <button className="general-btn">
+        
+            </button>
+        </div>
+    );
+}
+
+export default GeneralButton;
--- src/components/organisms/header/Header.jsx
+++ src/components/organisms/header/Header.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import GeneralButton from "../../atoms/button/generalButton/GeneralButton";
-import ThemeSwitchButton from "../../atoms/button/ThemeSwitchButton/ThemeSwitchButton";
-import LanguageSwitchButton from "../../atoms/button/languageSwitchButton/LanguageSwitchButton";
-import Navigation from "../../molecules/navigation/Navigation";
 
-function Header () {
+function Header() {
     return (
         <header className="header">
             <div className="menu-btn-container">
                 <GeneralButton />
-                <ThemeSwitchButton />
-                <LanguageSwitchButton />
             </div>
-            <Navigation />
             <div className="header-text">
                 {/* voir pour le contenu plus tard */}
             </div>
