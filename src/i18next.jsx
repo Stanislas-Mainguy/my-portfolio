a9cec4bac1c2360cd8e6507984b1a7628f5299c0
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-09-18 13:24:32 +0200

    Changement adresse des fichiers de traduction

--- src/i18next.jsx
+++ src/i18next.jsx
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import translationEN from './locales/en/translation.json';
-import translationFR from './locales/fr/translation.json';
+import translationEN from './translation/en/translation.json';
+import translationFR from './translation/fr/translation.json';
 
 const resources = {
   en: { translation: translationEN },
